7f1342dfc7dc224edb8ffc4ad7de5f13d4d67347
rafael <rafaelhazevedo61@gmail.com>
2022-04-23 03:01:06 -0300

    aula 5 - parcial - break e debugger

--- 8-loops.js
+++ 8-loops.js
@@ -9,7 +9,7 @@ const listaDeDestinos = new Array(
 const idadeComprador = 15;
 const estaAcompanhada = true;
 let temPassagemComprada = false;
-const destino = "Rio de Janeiro";
+const destino = "São Paulo";
 
 console.log("\n Destinos possíveis")
 console.log(listaDeDestinos)
@@ -17,14 +17,18 @@ console.log(listaDeDestinos)
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
 while(contador < listaDeDestinos.length){
+
     if(listaDeDestinos[contador] == destino){
         console.log("Destino existe")
-    } else{
-        console.log("Destino não existe")
-    }
+        destinoExiste = true;
+        break;
+    } 
     contador += 1;
 }
+
+console.log("Destino existe: ",destinoExiste)
 
 
 
