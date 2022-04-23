7ca7fcb9afe8a74680fa08d00e5b21ca9fb5009d
rafael <rafaelhazevedo61@gmail.com>
2022-04-23 02:43:11 -0300

    aula 4 - completa - operadores logicos encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -7,12 +7,13 @@ const listaDeDestinos = new Array(
 )
 
 const idadeComprador = 15;
-const estaAcompanhada = false;
+const estaAcompanhada = true;
+const temPassagemComprada = true;
 
 console.log("Destinos possíveis")
 console.log(listaDeDestinos)
 
-if (idadeComprador > 20) {
+/* if (idadeComprador > 20) {
     console.log("Comprador maior de idade")
     listaDeDestinos.splice(1, 1) // (a partir da posicao x,deletar x elementos), removendo item
 } else {
@@ -23,7 +24,22 @@ if (idadeComprador > 20) {
     } else {
         console.log("Não é maior de idade e não posso vender")
     }
+} */
+
+if (idadeComprador >= 18 || estaAcompanhada == true){
+    console.log("Boa viagem!!")
+    listaDeDestinos.splice(2,1)
+} else {
+    console.log("Não é maior de idade e não posso vender")
 }
+
+console.log("Embarque: \n \n")
+if(idadeComprador > 18 && temPassagemComprada){
+    console.log("Boa viagem!")
+} else {
+    console.log("Você não pode embarcar")
+}
+
 
 console.log(listaDeDestinos)
 
