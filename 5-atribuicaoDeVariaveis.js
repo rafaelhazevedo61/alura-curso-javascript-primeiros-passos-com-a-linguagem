08134e04ee1580afc868a00009d46d935a067f30
rafael <rafaelhazevedo61@gmail.com>
2022-04-23 02:17:16 -0300

    aula 2 - completa -atribuição e imutabilidade

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,16 @@
+console.log("Atribuição de Variáveis")
+
+const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+console.log (primeiroNome + sobrenome)
+console.log (primeiroNome,sobrenome)
+console.log (primeiroNome +" "+ sobrenome)
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto)
