2c18e12b95cabbe6bcbbc7fbf935d406721eed4e
rafael <rafaelhazevedo61@gmail.com>
2022-04-23 01:30:11 -0300

    aula 1 - completa - guardando valores na memoria do computador

--- meuPrimeiroPrograma.js
+++ meuPrimeiroPrograma.js
@@ -1,1 +1,14 @@
-console.log("Meu primeiro programa");
+//ATALHO: SHIFT + ALT + SETA -> COPIA A LINHA PARA BAIXO
+
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+
+console.log(idadeSomada);
