b1def7e0844eb2854fdcc45badd3a4b0b1e2e4dd
rafael <rafaelhazevedo61@gmail.com>
2022-04-23 02:36:30 -0300

    aula 4 - parcial - ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -1,20 +1,28 @@
 console.log(`Trabalhando com listas`)
 
-const listaDeDestinos = new Array (
+const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`
 )
 
 const idadeComprador = 15;
+const estaAcompanhada = false;
+
 console.log("Destinos possíveis")
 console.log(listaDeDestinos)
 
-if(idadeComprador > 20){
+if (idadeComprador > 20) {
     console.log("Comprador maior de idade")
-    listaDeDestinos.splice(1,1) // (a partir da posicao x,deletar x elementos), removendo item
+    listaDeDestinos.splice(1, 1) // (a partir da posicao x,deletar x elementos), removendo item
 } else {
-    console.log("Comprador menor de idade")
+    //A pessoa é menor de idade
+    if (estaAcompanhada) {
+        console.log("Comprador está acompanhado")
+        listaDeDestinos.splice(1, 1) // (a partir da posicao x,deletar x elementos), removendo item
+    } else {
+        console.log("Não é maior de idade e não posso vender")
+    }
 }
 
 console.log(listaDeDestinos)
