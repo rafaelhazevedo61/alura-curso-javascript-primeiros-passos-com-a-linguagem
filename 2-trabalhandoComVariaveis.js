87ad0c6cf49e96a6e4467d7d451e895007a0f56e
rafael <rafaelhazevedo61@gmail.com>
2022-04-23 01:58:51 -0300

    aula 2 - parcial - boas práticas com variáveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis") <-- nao funciona, dá erro
+//JS é case sensitive
+console.log("Trabalhando com variáveis")
+
+const idade = 29;
+const nome = "Ricardo";
+let ano = 2020;
+
+console.log(idade);
